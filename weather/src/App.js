73284d38f07a488151dfc8348ea217892f165e1f
Kha Truong <khatruong2009@gmail.com>
2020-09-25 15:12:29 -0400

    having trouble getting API

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 const api = {
   key: "aa208507f988bebecc6fe48cf95cff6e",
-  base: "http://api.openweathermap.org/data/2.5/"
+  base: "https://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&APPID${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+          console.log(weather);
+        });
+    }
+  }
 
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -28,6 +43,9 @@ function App() {
             type="text"
             className="search-bar"
             placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
           />
         </div>
         <div className="location-box">
